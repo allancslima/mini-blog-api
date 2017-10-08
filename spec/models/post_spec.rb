9410b8f5488a1f8683ca8744a5d9bf8f72856a0c
Allan Lima <allancslima@gmail.com>
2017-10-08 20:45:04 -0300

    Adding association with comments for post model and test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
 	let(:post) { build(:post) }
 
+	it { is_expected.to have_many(:comments).dependent(:destroy) }
+
 	it { is_expected.to validate_presence_of(:title) }
 
 	it { is_expected.to respond_to(:title) }
