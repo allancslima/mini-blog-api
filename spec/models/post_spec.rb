026c882487f26869fe85441ed0a3471cad5f14a6
Allan Lima <allancslima@gmail.com>
2017-10-08 13:41:04 -0300

    Adding post model with tests

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+	let(:post) { build(:post) }
+
+	it { is_expected.to validate_presence_of(:title) }
+
+	it { is_expected.to respond_to(:title) }
+	it { is_expected.to respond_to(:content) }
+end
