19318aa5a74d4647ca3e917a6fc5857a00474c32
Allan Lima <allancslima@gmail.com>
2017-10-09 00:43:34 -0300

    Adding show action to the comments controller with tests

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,12 @@ class Api::V1::CommentsController < ApplicationController
 		render json: { comments: comments }, status: 200
 	end
 
+	def show
+		begin
+			comment = current_post.comments.find(params[:comment_id])
+			render json: comment, status: 200
+		rescue
+			head 404
+		end
+	end
 end
--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -27,4 +27,31 @@ RSpec.describe 'Comments API' do
 		end
 	end
 
+
+	describe 'GET /posts/:post_id/comments/:comment_id' do
+		let(:comment) { create(:comment, post_id: post.id) }
+		let(:comment_id) { comment.id }
+		before do
+			get "/posts/#{post.id}/comments/#{comment.id}", params: { post_id: post.id, comment_id: comment_id }, headers: headers
+		end
+
+		context 'when the comment exists' do
+			it 'returns status code 200' do
+				expect(response).to have_http_status(200)
+			end
+
+			it 'returns the json data for the comment' do
+				expect(json_body[:email]).to eq(comment.email)
+			end
+		end
+
+		context 'when the comment does not exist' do
+			let(:comment_id) { -1 }
+
+			it 'returns status code 404' do
+				expect(response).to have_http_status(404)
+			end
+		end
+	end
+
 end
